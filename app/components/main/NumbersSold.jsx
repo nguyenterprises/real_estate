898673d04ee8c9c22336a71a673f1d2fe3597144
Hanh Nguyen <hanh@nguyenterprises.work>
2023-09-06 00:10:22 +0000

    final NumbersSold link update

--- app/components/main/NumbersSold.jsx
+++ app/components/main/NumbersSold.jsx
@@ -62,19 +62,19 @@ const NumbersSold = () => {
                 number="94"
                 address="924 Bel Air Rd"
                 delay="2"
-                link="/listing/924-bel-air-rd-los-angeles-california-90077-sold"
+                link="/listing/924-bel-air-rd-los-angeles-california-90077-na"
               />
               <SoldNumber
                 number="70"
                 address="1181 Hillcrest"
                 delay="2.75"
-                link="/listing/1181-n-hillcrest-rd-beverly-hills-90210-sold"
+                link="/listing/1181-n-hillcrest-rd-beverly-hills-90210-na"
               />
               <SoldNumber
                 number="29"
                 address="53 Beverly Park"
                 delay="3.25"
-                link="/listing/53-beverly-parkway-beverly-hills-california-sold"
+                link="/listing/53-beverly-parkway-beverly-hills-california-na"
               />
             </div>
           </div>
